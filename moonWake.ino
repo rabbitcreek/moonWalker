faa8ef08ad1b284c255838865a4378f74e98e38b
rabbitcreek <rabbitcreek@gmail.com>
2024-12-26 15:50:23 -1000

    Add files via upload
    adds sleep for one minute

--- moonWake.ino
+++ moonWake.ino
@@ -3,6 +3,11 @@
 #include "RTClib.h"
 #define DATA_PIN2 D8
 #define DATA_PIN D9
+#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  60        /* Time ESP32 will go to sleep (in seconds) */
+
+RTC_DATA_ATTR int bootCount = 0;
+
 int year;
 int day;
 int month;
@@ -20,7 +25,8 @@ int servoPin = D7;
 CRGB leds[NUM_LEDS];
 CRGB leds2[NUM_LEDS];
 void setup() {
-  Serial.begin();
+  Serial.begin(115200);
+  delay(1000);
   ESP32PWM::allocateTimer(0);
 	ESP32PWM::allocateTimer(1);
 	ESP32PWM::allocateTimer(2);
@@ -54,7 +60,19 @@ void setup() {
     
    Serial.print("Moon Number: ");
    Serial.println(moonNum);
-    
+    ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+
+  /*
+  First we configure the wake up source
+  We set our ESP32 to wake up every 5 seconds
+  */
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Seconds"); 
 
 }
   void loop(){
@@ -64,9 +82,10 @@ void setup() {
   Serial.println((char*)moonPhaseText(moonNum));
   showMoon();
   Serial.print("Moon Number: ");
-   Serial.println(moonNum);
+  Serial.println(moonNum);
   myservo.write(range); 
   delay(500);
+  sleepy();
   }
   void printTime(){//call this to print the current time on RTC
      
@@ -216,7 +235,7 @@ char* moonPhaseText(int moonNum)
   fill = 1;
   } else {
     hourCorrect = now.hour() - 19;
-    
+    hourCorrect = constrain(hourCorrect,0,3);
     if(moonNum < 16){
        range = map( moonNum,1,15,0,180);
       fill = 2;
@@ -259,5 +278,25 @@ char* moonPhaseText(int moonNum)
   FastLED.show();
   delay(1000);
 }
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
 
+  wakeup_reason = esp_sleep_get_wakeup_cause();
 
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
+void sleepy(){
+  Serial.println("Going to sleep now");
+  delay(30000);
+  //Serial.flush(); 
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed");
+}
